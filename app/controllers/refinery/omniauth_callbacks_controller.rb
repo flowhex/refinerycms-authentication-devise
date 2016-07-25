b7c144935b7c11579c7a500cd3644aab310a9157
aurelien petit <aurelien.petitdev@gmail.com>
2016-07-25 09:45:55 +0000

    wrong location for the callback

---
+++ app/controllers/refinery/omniauth_callbacks_controller.rb
@@ -0,0 +1,6 @@
+class OmniauthCallbacksController < ::Devise::OmniauthCallbacksController
+  def wechat_qiye
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
+  end
+end
