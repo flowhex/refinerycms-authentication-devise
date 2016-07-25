547b103cf6723a0b9b9042d3b2a1bcac76c4cd61
aurelien petit <aurelien.petitdev@gmail.com>
2016-07-25 07:41:44 +0000

    fixed the name of the table for table migration

--- db/migrate/20160725065712_add_columns_to_refinery_users.rb
+++ db/migrate/20160725065712_add_columns_to_refinery_users.rb
@@ -1,6 +1,6 @@
 class AddColumnsToRefineryUsers < ActiveRecord::Migration
   def change
-    add_column :refinery_authentication_devise_users, :provider, :string
-    add_column :refinery_authentication_devise_users, :uid, :string
+    add_column :refinery_users, :provider, :string
+    add_column :refinery_users, :uid, :string
   end
 end
