2d61937a626d92dbe796e132b7d64598732893f3
aurelien petit <aurelien.petitdev@gmail.com>
2016-07-25 08:43:43 +0000

    add callback controller corrected

---
+++ app/controllers/refinery/authentication/devise/callbacks_controller.rb
@@ -0,0 +1,12 @@
+module Refinery
+  module Authentication
+    module Devise
+      class CallbacksController < ::Devise::OmniauthCallbacksController
+        def wechat_qiye
+          @user = User.from_omniauth(request.env["omniauth.auth"])
+          sign_in_and_redirect @user
+        end
+      end
+    end
+  end
+end
